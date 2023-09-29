e9a73fa844c8b87bd3416e739f8de2eb789aa5a4
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-29 15:11:58 +0700

    debug, delete user.password

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,11 +1,21 @@
 import { createContext, useState } from "react";
+import axios from "../config/axios";
+import { addAccessToken } from "../utils/local-storage";
 
 export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
   const [authUser, setAuthUser] = useState(null);
-  const login = (credential) => {
-    console.log(credential)
+  
+  const login = async credential => {
+    try {
+        const res = await axios.post('/auth/login', credential)
+        console.log(res)
+        addAccessToken(res.data.accessToken)
+        setAuthUser(res.data.user)
+    } catch(err) {
+        console.log(err)
+    }
   };
   return (
     <AuthContext.Provider value={{ login }}>{children}</AuthContext.Provider>
