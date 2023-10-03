8bc4f7c38309f15d6140d2ad62c0dbc9eedcd340
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-03 10:30:47 +0700

    add logout

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "../config/axios";
-import { addAccessToken, getAccessToken } from "../utils/local-storage";
+import { addAccessToken, getAccessToken, removeAccessToken } from "../utils/local-storage";
 
 export const AuthContext = createContext();
 
@@ -38,8 +38,14 @@ export default function AuthContextProvider({ children }) {
     setAuthUser(res.data.user);
   };
 
+const logout = () => {
+  removeAccessToken()
+  setAuthUser(null)
+}
+
+
   return (
-    <AuthContext.Provider value={{ login, authUser, initialLoading, register }}>
+    <AuthContext.Provider value={{ login, authUser, initialLoading, register, logout }}>
       {children}
     </AuthContext.Provider>
   );
