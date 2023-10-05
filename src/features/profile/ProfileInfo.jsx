763a504331c77dfea8d286304db85b751386577d
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-05 17:22:22 +0700

    add display user's friend to UI

--- src/features/profile/ProfileInfo.jsx
+++ src/features/profile/ProfileInfo.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import AuthUserAction from "./AuthUserAction";
 import FriendAction from "./FriendAction";
@@ -9,12 +10,15 @@ export default function ProfileInfo({
   profileUser,
   statusWithAuthUser,
   setStatusWithAuthUser,
+  profileFriends,
 }) {
   const mappingObj = {
     AUTH_USER: <AuthUserAction />,
     UNKNOWN: <UnknownAction setStatusWithAuthUser={setStatusWithAuthUser} />,
-    FRIEND: <FriendAction setStatusWithAuthUser={setStatusWithAuthUser}/>,
-    REQUESTER: <RequesterAction setStatusWithAuthUser = {setStatusWithAuthUser} />,
+    FRIEND: <FriendAction setStatusWithAuthUser={setStatusWithAuthUser} />,
+    REQUESTER: (
+      <RequesterAction setStatusWithAuthUser={setStatusWithAuthUser} />
+    ),
     RECEIVER: <ReceiverAction setStatusWithAuthUser={setStatusWithAuthUser} />,
   };
 
@@ -31,13 +35,14 @@ export default function ProfileInfo({
           {profileUser.firstName} {profileUser.lastName}
         </h2>
         <span className=" block text-gray-500 font-semibold mb-2">
-          6 friends
+          {profileFriends.length} Friends
         </span>
         <div className=" flex -space-x-2">
-          <Avatar className=" h-8" />
-          <Avatar className=" h-8" />
-          <Avatar className=" h-8" />
-          <Avatar className=" h-8" />
+          {profileFriends.map((el) => (
+            <Link key={el.id} to={`/profile/${el.id}`}>
+              <Avatar className=" h-8" src={el.profileImage} />
+            </Link>
+          ))}
         </div>
       </div>
       <div>{mappingObj[statusWithAuthUser]}</div>
