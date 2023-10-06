6430244943edfeea4f81268624ed31db823f162a
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-06 15:24:48 +0700

    modify post content, add TimeAgo

--- src/features/post/PostContent.jsx
+++ src/features/post/PostContent.jsx
@@ -1,13 +1,12 @@
-export default function PostContent() {
+export default function PostContent({ message, image }) {
   return (
     <div className=" py-2 flex flex-col gap-4">
-      <span className=" text-sm">PostContent</span>
-      <div className=" mx-4">
-        <img
-          src="https://service-cdn.coconala.com/resize/1220/1240/service_images/original/1f4fff01-4215506.jpg"
-          alt=""
-        />
-      </div>
+      {message && <span className=" text-sm">{message}</span>}
+      {image && (
+        <div className=" mx-4">
+          <img src={image} alt="post" />
+        </div>
+      )}
     </div>
   );
 }
--- src/features/post/PostItem.jsx
+++ src/features/post/PostItem.jsx
@@ -2,11 +2,11 @@ import PostHeader from "./PostHeader";
 import PostContent from './PostContent'
 import PostFooter from "./PostFooter";
 
-export default function PostItem() {
+export default function PostItem({postObj}) {
   return (
     <div className=" bg-white px-4 pt-3 border shadow rounded-lg">
-        <PostHeader/>
-        <PostContent />
+        <PostHeader postObj={postObj}/>
+        <PostContent message={postObj.message} image={postObj.image} />
         <PostFooter />
         </div>
   )
