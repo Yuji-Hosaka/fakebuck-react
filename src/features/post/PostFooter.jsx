b359a921df6fe0adf12a1298383c0ee5227a87aa
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-06 19:00:58 +0700

    modify post layout, add blue color button on the liked post

--- src/features/post/PostFooter.jsx
+++ src/features/post/PostFooter.jsx
@@ -1,26 +1,35 @@
 import { MessageIcon, ThumbsUpAltIcon, ThumbsUpIcon } from "../../icons";
 import ActionButton from "./ActionButton";
+import { useAuth } from "../../hooks/use-auth";
 
-export default function PostFooter() {
+export default function PostFooter({ postObj }) {
+  const { totalLike, likes } = postObj;
+  const { authUser } = useAuth();
+
+  const isLiked = likes.find((el) => el.userId === authUser.id);
   return (
     <div>
       <div className=" flex justify-between items-center pb-2">
-        <div className=" flex gap-1 items-center">
-          <div className=" bg-blue-500 h-4 w-4 rounded-full flex items-center justify-center">
-            <ThumbsUpIcon />
+        {totalLike > 0 && (
+          <div className=" flex gap-1 items-center">
+            <div className=" bg-blue-500 h-4 w-4 rounded-full flex items-center justify-center">
+              <ThumbsUpIcon />
+            </div>
+            <span className=" text-sm text-gray-500">{totalLike}</span>
           </div>
-          <span className=" text-sm text-gray-500">3</span>
-        </div>
+        )}
+
         <span className=" text-gray-500 hover:underline cursor-pointer">
           8 comments
         </span>
       </div>
       <hr />
       <div className=" flex gap-1 py-1">
-        
-        <ActionButton>
+        <ActionButton active={isLiked}>
           <div className=" flex justify-center gap-2">
-            <ThumbsUpAltIcon className=" fill-gray-500" />
+            <ThumbsUpAltIcon
+              className={isLiked ? "fill-blue-500" : "fill-gray-500"}
+            />
             <span>Like</span>
           </div>
         </ActionButton>
