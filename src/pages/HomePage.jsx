3760c6ce9cbbcb3aa7a1d38d0108bdcbd23719ed
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-09 14:30:08 +0700

    update state at post button UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 import CreatePostButton from "../features/post/CreatePostButton";
 import PostList from "../features/post/PostList";
 import axios from "../config/axios";
+import { all } from "axios";
 
 export default function HomePage() {
   const [allPost, setAllPost] = useState([]);
+
+  const createPost = async (data) => {
+    const res = await axios.post("/post", data);
+    const newPost = res.data.post
+    setAllPost([newPost, ...allPost])
+  };
+
   useEffect(() => {
     axios
       .get("/post/friend")
@@ -17,7 +25,7 @@ export default function HomePage() {
   }, []);
   return (
     <div className=" max-w-[44rem] mx-auto px-8 py-6 flex flex-col gap-4">
-      <CreatePostButton />
+      <CreatePostButton createPost={createPost} />
       <PostList allPost={allPost} />
     </div>
   );
